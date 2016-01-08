d02c2690a5f8073958950dd53d18e25db766d859
Alec Lombardo <alec.lombardo@gmail.com>
2016-01-07 20:32:12 -0600

    Removed overlay and centered with dynamic width

--- js/jquery.twentytwenty.js
+++ js/jquery.twentytwenty.js
@@ -9,25 +9,19 @@
       var sliderOrientation = options.orientation;
       var beforeDirection = (sliderOrientation === 'vertical') ? 'down' : 'left';
       var afterDirection = (sliderOrientation === 'vertical') ? 'up' : 'right';
-      
-      
+
+
       container.wrap("<div class='twentytwenty-wrapper twentytwenty-" + sliderOrientation + "'></div>");
-      container.append("<div class='twentytwenty-overlay'></div>");
       var beforeImg = container.find("img:first");
       var afterImg = container.find("img:last");
       container.append("<div class='twentytwenty-handle'></div>");
       var slider = container.find(".twentytwenty-handle");
-      slider.append("<span class='twentytwenty-" + beforeDirection + "-arrow'></span>");
-      slider.append("<span class='twentytwenty-" + afterDirection + "-arrow'></span>");
       container.addClass("twentytwenty-container");
       beforeImg.addClass("twentytwenty-before");
       afterImg.addClass("twentytwenty-after");
-      
-      var overlay = container.find(".twentytwenty-overlay");
-      overlay.append("<div class='twentytwenty-before-label'></div>");
-      overlay.append("<div class='twentytwenty-after-label'></div>");
 
       var calcOffset = function(dimensionPct) {
+        container.css("width", "auto");
         var w = beforeImg.width();
         var h = beforeImg.height();
         return {
@@ -46,21 +40,25 @@
           beforeImg.css("clip", "rect(0,"+offset.cw+","+offset.h+",0)");
     	}
         container.css("height", offset.h);
+        container.css("width", offset.w);
       };
 
       var adjustSlider = function(pct) {
         var offset = calcOffset(pct);
         slider.css((sliderOrientation==="vertical") ? "top" : "left", (sliderOrientation==="vertical") ? offset.ch : offset.cw);
         adjustContainer(offset);
-      }
+      };
 
+      $( window ).resize(function() {
+        adjustContainer(sliderPct);
+      });
       $(window).on("resize.twentytwenty", function(e) {
         adjustSlider(sliderPct);
       });
 
       var offsetX = 0;
       var imgWidth = 0;
-      
+
       slider.on("movestart", function(e) {
         if (((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) && sliderOrientation !== 'vertical') {
           e.preventDefault();
@@ -71,8 +69,8 @@
         container.addClass("active");
         offsetX = container.offset().left;
         offsetY = container.offset().top;
-        imgWidth = beforeImg.width(); 
-        imgHeight = beforeImg.height();          
+        imgWidth = beforeImg.width();
+        imgHeight = beforeImg.height();
       });
 
       slider.on("moveend", function(e) {
